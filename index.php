713a92323f4b1d927c530cd96cad118a19245e1a
HunterBuxton <huntercbuxton@icloud.com>
2020-02-19 12:08:35 -0700

    a readme and template folders/files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='UTF-8'/>
+	<title>Home: Matrix Workflow</title>
+	<link rel='stylesheet' href='css/styles.css'/>	
+	<script src="scripts/scripts.js"></script>
+</head>
+<body>
+	<header></header>
+	<main></main>
+	<footer></footer>
+</body>
+</html>
